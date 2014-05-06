e6fbd616a1f80f964856fba44f19967031081f03
mansar@asicentral.com <mansar@asicentral.com>
2014-05-06 02:58:12 -0400

    Added file for finding user password and user role. ONLY ROLE_CUSTOMER is allowed in get API

--- src/main/java/com/asi/core/security/CustomAsiUserDetailService.java
+++ src/main/java/com/asi/core/security/CustomAsiUserDetailService.java
@@ -3,6 +3,11 @@ package com.asi.core.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -12,16 +17,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configuration
+@PropertySource("classpath:users.properties")
 public class CustomAsiUserDetailService implements UserDetailsService {
 
-	
+	@Resource
+	private Environment environment;
 	@Override
 	public UserDetails loadUserByUsername(final String userName)
 			throws UsernameNotFoundException {
+		String userPassword = environment.getProperty(String.format("user.%s.pass",userName));
+		String userRole = environment.getProperty(String.format("user.%s.role",userName));
 		List<GrantedAuthority> gr = new ArrayList<GrantedAuthority>();
-		SimpleGrantedAuthority grau = new SimpleGrantedAuthority("ROLE_CUSTOMER");
+		SimpleGrantedAuthority grau = new SimpleGrantedAuthority(userRole);
 		gr.add(grau);
-		UserDetails user = new User(userName, "password", true, true, true, true, gr);
+		UserDetails user = new User(userName, userPassword, true, true, true, true, gr);
 		return user;
 	}
 
