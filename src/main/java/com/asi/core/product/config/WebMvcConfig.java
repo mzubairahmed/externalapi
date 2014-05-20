f35d73168d214168fab34b2254919db4dcdaacb4
mansar@asicentral.com <mansar@asicentral.com>
2014-05-20 14:06:40 -0400

    reverted

--- src/main/java/com/asi/core/product/config/WebMvcConfig.java
+++ src/main/java/com/asi/core/product/config/WebMvcConfig.java
@@ -1,25 +1,30 @@
 package com.asi.core.product.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.asi.service.core.exception.ErrorMessageFactory;
+import com.asi.service.core.exception.ErrorMessageHandlerExceptionResolver;
+import com.asi.service.core.exception.HttpMediaTypeNotSupportedExceptionErrorMessageFactory;
 
 
 
-//@Configuration
+@Configuration
 //@PropertySource({"classpath:dev-environment.properties", "classpath:velocity-import.properties"})
-public class WebMvcConfig{
-//	extends WebMvcConfigurationSupport {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+	@Bean(name="errorMessageHandlerExceptionResolver")
+    public ErrorMessageHandlerExceptionResolver createErrorMessageHandlerExceptionResolver() {
+		ErrorMessageHandlerExceptionResolver r =
+              new ErrorMessageHandlerExceptionResolver();
+		ErrorMessageFactory<?>[] errorMessageFactories = new ErrorMessageFactory<?>[1];
+		HttpMediaTypeNotSupportedExceptionErrorMessageFactory hmtException =new HttpMediaTypeNotSupportedExceptionErrorMessageFactory();
+		errorMessageFactories[0]=hmtException;
 
-//	@Bean(name="errorMessageHandlerExceptionResolver")
-//    public ErrorMessageHandlerExceptionResolver createErrorMessageHandlerExceptionResolver() {
-//		ErrorMessageHandlerExceptionResolver r =
-//              new ErrorMessageHandlerExceptionResolver();
-//		ErrorMessageFactory<?>[] errorMessageFactories = new ErrorMessageFactory<?>[1];
-//		HttpMediaTypeNotSupportedExceptionErrorMessageFactory hmtException =new HttpMediaTypeNotSupportedExceptionErrorMessageFactory();
-//		errorMessageFactories[0]=hmtException;
-//
-//		r.setErrorMessageFactories(errorMessageFactories);
-//
-//        return r;
-//    }
+		r.setErrorMessageFactories(errorMessageFactories);
+
+        return r;
+    }
 //	 @Bean
 //	 public RequestMappingHandlerMapping requestMappingHandlerMapping() {
 //	  RequestMappingHandlerMapping handlerMapping = super.requestMappingHandlerMapping();
