dbb489a73b65c064e127fdb07c0beccd78edad12
mansar@asicentral.com <mansar@asicentral.com>
2014-05-20 17:58:09 -0400

    Service HealthCheck

--- src/main/java/com/asi/service/product/vo/package-info.java
+++ src/main/java/com/asi/service/product/vo/package-info.java
@@ -1,7 +1,9 @@
 @javax.xml.bind.annotation.XmlSchema(
     namespace = "http://www.asicentral.com/schema/product",
     xmlns = {
-          @javax.xml.bind.annotation.XmlNs(prefix = "p", namespaceURI ="http://www.asicentral.com/schema/product")
+          @javax.xml.bind.annotation.XmlNs(prefix = "p", namespaceURI ="http://www.asicentral.com/schema/product"),
+          @javax.xml.bind.annotation.XmlNs(prefix = "s", namespaceURI ="http://www.asicentral.com/schema/health")
             }, 
     elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-    package com.asi.service.product.vo;
+
+package com.asi.service.product.vo;
