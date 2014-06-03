d820d29e9b2d7647e90341be6986bf00269ee04c
Muqtadir Ansar <mansar@asicentral.com>
2014-06-02 23:08:49 -0700

    xsd generation

--- src/main/java/com/asi/service/product/vo/Constrain.java
+++ src/main/java/com/asi/service/product/vo/Constrain.java
@@ -1,13 +1,14 @@
 package com.asi.service.product.vo;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @XmlRootElement(namespace = "http://www.asicentral.com/schema/product" , name = "constrain")
-@XmlType(propOrder={"isAllowBasePrice","isAllowUpcharge","isFlag","isProductNumberAssignmentAllowed","isMediaAssignmentAllowed","isSupplierSpecific"})
-@JsonPropertyOrder({"isAllowBasePrice","isAllowUpcharge","isFlag","isProductNumberAssignmentAllowed","isMediaAssignmentAllowed","isSupplierSpecific"})
+@XmlType(propOrder={"allowBasePrice","allowUpcharge","flag","productNumberAssignmentAllowed","mediaAssignmentAllowed","supplierSpecific"})
+@JsonPropertyOrder({"allowBasePrice","allowUpcharge","flag","productNumberAssignmentAllowed","mediaAssignmentAllowed","supplierSpecific"})
 
 public class Constrain {
 	private boolean isAllowBasePrice;
@@ -16,43 +17,50 @@ public class Constrain {
 	private boolean isProductNumberAssignmentAllowed;
 	private boolean isMediaAssignmentAllowed;
 	private boolean isSupplierSpecific;
-	
-	public boolean isSupplierSpecific() {
-		return isSupplierSpecific;
-	}
-	public void setSupplierSpecific(boolean isSupplierSpecific) {
-		this.isSupplierSpecific = isSupplierSpecific;
-	}
+	@XmlElement(name="AllowBasePrice")
 	public boolean isAllowBasePrice() {
 		return isAllowBasePrice;
+	}
+	@XmlElement(name="AllowUpcharge")
+	public boolean isAllowUpcharge() {
+		return isAllowUpcharge;
+	}
+	@XmlElement(name="Flag")
+	public boolean isFlag() {
+		return isFlag;
+	}
+	@XmlElement(name="ProductNumberAssignmentAllowed")
+	public boolean isProductNumberAssignmentAllowed() {
+		return isProductNumberAssignmentAllowed;
+	}
+	@XmlElement(name="MediaAssignmentAllowed")
+	public boolean isMediaAssignmentAllowed() {
+		return isMediaAssignmentAllowed;
+	}
+	@XmlElement(name="SupplierSpecific")
+	public boolean isSupplierSpecific() {
+		return isSupplierSpecific;
 	}
 	public void setAllowBasePrice(boolean isAllowBasePrice) {
 		this.isAllowBasePrice = isAllowBasePrice;
 	}
-	public boolean isAllowUpcharge() {
-		return isAllowUpcharge;
-	}
 	public void setAllowUpcharge(boolean isAllowUpcharge) {
 		this.isAllowUpcharge = isAllowUpcharge;
 	}
-	public boolean isFlag() {
-		return isFlag;
-	}
 	public void setFlag(boolean isFlag) {
 		this.isFlag = isFlag;
-	}
-	public boolean isProductNumberAssignmentAllowed() {
-		return isProductNumberAssignmentAllowed;
 	}
 	public void setProductNumberAssignmentAllowed(
 			boolean isProductNumberAssignmentAllowed) {
 		this.isProductNumberAssignmentAllowed = isProductNumberAssignmentAllowed;
 	}
-	public boolean isMediaAssignmentAllowed() {
-		return isMediaAssignmentAllowed;
-	}
 	public void setMediaAssignmentAllowed(boolean isMediaAssignmentAllowed) {
 		this.isMediaAssignmentAllowed = isMediaAssignmentAllowed;
 	}
+	public void setSupplierSpecific(boolean isSupplierSpecific) {
+		this.isSupplierSpecific = isSupplierSpecific;
+	}
+	
+
 	
 }
