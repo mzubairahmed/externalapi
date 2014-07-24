0667986b6d4d322e8bb66f1f8d2e5a1d881d1027
Rahul <RKrishna@asicentral.com>
2014-07-24 14:54:06 +0530

    Options criteria lookup related changes implemented.

--- src/main/java/com/asi/ext/api/product/transformers/ImportTransformer.java
+++ src/main/java/com/asi/ext/api/product/transformers/ImportTransformer.java
@@ -18,6 +18,7 @@ import com.asi.ext.api.product.criteria.processor.ProductKeywordProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductMateriaProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductMediaItemProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductOptionProcessor;
+import com.asi.ext.api.product.criteria.processor.ProductOptionsProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductOriginProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductPackageProcessor;
 import com.asi.ext.api.product.criteria.processor.ProductSelectedComplianceCertProcessor;
@@ -314,6 +315,8 @@ public class ImportTransformer {
             existingCriteriaSetMap.remove(ApplicationConstants.CONST_PRODUCTION_TIME_CRITERIA_CODE);
         }
 
+        ProductOptionsProcessor optionsProcessor = new ProductOptionsProcessor();
+        optionsProcessor.tryLookupData("PROP");
         // Merge all updated ProductCriteriaSets into product configuration and set back to list
         ProductConfiguration updatedProductConfiguration = new ProductConfiguration();
         updatedProductConfiguration.setConfigId(configId);
@@ -835,95 +838,95 @@ public class ImportTransformer {
      * minQuantity = criteriaCodesAry[i];
      * }
      * }
-     * 
-     * // Option Type Starts Here
-     * 
-     * if (null != optionType && !optionType.equalsIgnoreCase("null") && !optionType.equals("")
-     * && CommonUtilities.isUpdateNeeded(optionType, true)) {
-     * Map<String, List<ProductCriteriaSets>> newOptionsCriteriaSets = new HashMap<>();
-     * 
-     * optionType = CommonUtilities.removeUpdateCharsString(optionType);
-     * LOGGER.info("Option Type Transformation Starts :" + optionType);
-     * String[] optionTypeAry = optionType.split(",");
-     * String[] optionValuesAry = CommonUtilities.removeUpdateCharsString(optionValues).split("\\|");
-     * String[] optionNamesAry = CommonUtilities.removeUpdateCharsString(optionNames).split("\\|");
-     * String[] reqForOrderAry = CommonUtilities.removeUpdateCharsString(reqForOrder).split("\\|");
-     * String[] isMultipleChoiceAllowedAry = CommonUtilities.removeUpdateCharsString(isMultipleChoiceAllowed)
-     * .split("\\|");
-     * 
-     * int optionValueCntr = 0;
-     * for (String crntOptionType : optionTypeAry) {
-     * if (crntOptionType.equalsIgnoreCase("Product Option")
-     * || crntOptionType.equalsIgnoreCase("Imprint Option")
-     * || crntOptionType.equalsIgnoreCase("Shipping Option")) {
-     * if (optionValuesAry.length > optionValueCntr && null != optionValuesAry[optionValueCntr]
-     * && !optionValuesAry[optionValueCntr].equalsIgnoreCase("null")
-     * && !optionValuesAry[optionValueCntr].isEmpty()) {
-     * ProductCriteriaSets prdCriteriaSets = productParser.addCriteriaSetForCriteriaCode(
-     * existingProduct, product, crntOptionType, optionValuesAry[optionValueCntr],
-     * optionNamesAry[optionValueCntr]);
-     * if (prdCriteriaSets != null && CommonUtilities.isValueNull(optionNamesAry[optionValueCntr])) {
-     * String temp = CommonUtilities.getValueFromCSV(optionValuesAry[optionValueCntr], 1);
-     * if (!CommonUtilities.isValueNull(temp)) {
-     * prdCriteriaSets.setCriteriaDetail(temp);
-     * }
-     * }
-     * if (null != prdCriteriaSets && cntr < productCriteriaSetsAry.length) {
-     * Boolean temp = CommonUtilities.getBooleanValueFromString(reqForOrderAry[optionValueCntr]);
-     * if (temp != null) {
-     * prdCriteriaSets.setIsRequiredForOrder(temp.toString());
-     * } else {
-     * productDataStore.addErrorToBatchLogCollection(product.getExternalProductId().trim(),
-     * ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
-     * "Invalid Req_for_order value " + reqForOrderAry[optionValueCntr]
-     * + " found for " + crntOptionType);
-     * }
-     * temp = CommonUtilities
-     * .getBooleanValueFromString(isMultipleChoiceAllowedAry[optionValueCntr]);
-     * 
-     * if (temp != null) {
-     * prdCriteriaSets.setIsMultipleChoiceAllowed(temp.toString());
-     * } else {
-     * productDataStore.addErrorToBatchLogCollection(product.getExternalProductId().trim(),
-     * ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
-     * "Invalid Can_order_only_one value "
-     * + isMultipleChoiceAllowedAry[optionValueCntr] + " found for "
-     * + crntOptionType);
-     * }
-     * // TODO : values
-     * prdCriteriaSets = ProductCompareUtil.multipleProductCriteriaSetCompareAndUpdate(product,
-     * prdCriteriaSets.getCriteriaCode(), prdCriteriaSets,
-     * optionsCriteriaSet.get(prdCriteriaSets.getCriteriaCode()), true);
-     * if (newOptionsCriteriaSets.get(prdCriteriaSets.getCriteriaCode()) == null) {
-     * newOptionsCriteriaSets.put(prdCriteriaSets.getCriteriaCode(),
-     * new ArrayList<ProductCriteriaSets>());
-     * }
-     * newOptionsCriteriaSets.get(prdCriteriaSets.getCriteriaCode()).add(prdCriteriaSets);
-     * // productCriteriaSetsAry[cntr] = prdCriteriaSets;
-     * cntr++;
-     * } else {
-     * // productCriteriaSetsAry = Arrays.copyOf(productCriteriaSetsAry,
-     * // productCriteriaSetsAry.length - 1);
-     * // cntr--;
-     * }
-     * }
-     * } else if (!CommonUtilities.isValueNull(crntOptionType)) {
-     * LOGGER.info("Invalid Option Type" + crntOptionType);
-     * productDataStore
-     * .addErrorToBatchLogCollection(product.getExternalProductId().trim(),
-     * ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE, "Invalid Option type "
-     * + crntOptionType);
-     * }
-     * optionValueCntr++;
-     * }
-     * optionsCriteriaSet = newOptionsCriteriaSets;
-     * LOGGER.info("Option Type Transformation End");
-     * } else if (CommonUtilities.isValueNull(optionType)) {
-     * optionsCriteriaSet = new HashMap<String, List<ProductCriteriaSets>>();
-     * } else if (!CommonUtilities.isUpdateNeeded(optionType)) {
-     * productOptionProcessor.registerExistingOptionValues(product.getExternalProductId(), optionsCriteriaSet);
-     * }
-     * 
+     
+     // Option Type Starts Here
+     
+     if (null != optionType && !optionType.equalsIgnoreCase("null") && !optionType.equals("")
+     && CommonUtilities.isUpdateNeeded(optionType, true)) {
+     Map<String, List<ProductCriteriaSets>> newOptionsCriteriaSets = new HashMap<>();
+     
+     optionType = CommonUtilities.removeUpdateCharsString(optionType);
+     LOGGER.info("Option Type Transformation Starts :" + optionType);
+     String[] optionTypeAry = optionType.split(",");
+     String[] optionValuesAry = CommonUtilities.removeUpdateCharsString(optionValues).split("\\|");
+     String[] optionNamesAry = CommonUtilities.removeUpdateCharsString(optionNames).split("\\|");
+     String[] reqForOrderAry = CommonUtilities.removeUpdateCharsString(reqForOrder).split("\\|");
+     String[] isMultipleChoiceAllowedAry = CommonUtilities.removeUpdateCharsString(isMultipleChoiceAllowed)
+     .split("\\|");
+     
+     int optionValueCntr = 0;
+     for (String crntOptionType : optionTypeAry) {
+     if (crntOptionType.equalsIgnoreCase("Product Option")
+     || crntOptionType.equalsIgnoreCase("Imprint Option")
+     || crntOptionType.equalsIgnoreCase("Shipping Option")) {
+     if (optionValuesAry.length > optionValueCntr && null != optionValuesAry[optionValueCntr]
+     && !optionValuesAry[optionValueCntr].equalsIgnoreCase("null")
+     && !optionValuesAry[optionValueCntr].isEmpty()) {
+     ProductCriteriaSets prdCriteriaSets = productParser.addCriteriaSetForCriteriaCode(
+     existingProduct, product, crntOptionType, optionValuesAry[optionValueCntr],
+     optionNamesAry[optionValueCntr]);
+     if (prdCriteriaSets != null && CommonUtilities.isValueNull(optionNamesAry[optionValueCntr])) {
+     String temp = CommonUtilities.getValueFromCSV(optionValuesAry[optionValueCntr], 1);
+     if (!CommonUtilities.isValueNull(temp)) {
+     prdCriteriaSets.setCriteriaDetail(temp);
+     }
+     }
+     if (null != prdCriteriaSets && cntr < productCriteriaSetsAry.length) {
+     Boolean temp = CommonUtilities.getBooleanValueFromString(reqForOrderAry[optionValueCntr]);
+     if (temp != null) {
+     prdCriteriaSets.setIsRequiredForOrder(temp.toString());
+     } else {
+     productDataStore.addErrorToBatchLogCollection(product.getExternalProductId().trim(),
+     ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
+     "Invalid Req_for_order value " + reqForOrderAry[optionValueCntr]
+     + " found for " + crntOptionType);
+     }
+     temp = CommonUtilities
+     .getBooleanValueFromString(isMultipleChoiceAllowedAry[optionValueCntr]);
+     
+     if (temp != null) {
+     prdCriteriaSets.setIsMultipleChoiceAllowed(temp.toString());
+     } else {
+     productDataStore.addErrorToBatchLogCollection(product.getExternalProductId().trim(),
+     ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
+     "Invalid Can_order_only_one value "
+     + isMultipleChoiceAllowedAry[optionValueCntr] + " found for "
+     + crntOptionType);
+     }
+     // TODO : values
+     prdCriteriaSets = ProductCompareUtil.multipleProductCriteriaSetCompareAndUpdate(product,
+     prdCriteriaSets.getCriteriaCode(), prdCriteriaSets,
+     optionsCriteriaSet.get(prdCriteriaSets.getCriteriaCode()), true);
+     if (newOptionsCriteriaSets.get(prdCriteriaSets.getCriteriaCode()) == null) {
+     newOptionsCriteriaSets.put(prdCriteriaSets.getCriteriaCode(),
+     new ArrayList<ProductCriteriaSets>());
+     }
+     newOptionsCriteriaSets.get(prdCriteriaSets.getCriteriaCode()).add(prdCriteriaSets);
+     // productCriteriaSetsAry[cntr] = prdCriteriaSets;
+     cntr++;
+     } else {
+     // productCriteriaSetsAry = Arrays.copyOf(productCriteriaSetsAry,
+     // productCriteriaSetsAry.length - 1);
+     // cntr--;
+     }
+      }
+      } else if (!CommonUtilities.isValueNull(crntOptionType)) {
+      LOGGER.info("Invalid Option Type" + crntOptionType);
+      productDataStore
+      .addErrorToBatchLogCollection(product.getExternalProductId().trim(),
+      ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE, "Invalid Option type "
+      + crntOptionType);
+      }
+      optionValueCntr++;
+      }
+      optionsCriteriaSet = newOptionsCriteriaSets;
+      LOGGER.info("Option Type Transformation End");
+      } else if (CommonUtilities.isValueNull(optionType)) {
+      optionsCriteriaSet = new HashMap<String, List<ProductCriteriaSets>>();
+      } else if (!CommonUtilities.isUpdateNeeded(optionType)) {
+      productOptionProcessor.registerExistingOptionValues(product.getExternalProductId(), optionsCriteriaSet);
+      }
+      
      * // Product Sample and Spec Sample
      * ProductSpecSampleProcessor specSampleProcessor = new ProductSpecSampleProcessor(-1401, configId);
      * ProductCriteriaSets specSampleCriteriaSet = specSampleProcessor.getCriteriaSetForMultiple(specSample,
