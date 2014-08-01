3c3d47e20aeefaf736c9229ed9d27c5594e20a48
Rahul <RKrishna@asicentral.com>
2014-08-01 12:32:11 +0530

    Some changes related to Product Number processing.

--- src/main/java/com/asi/ext/api/product/transformers/ProductNumberCriteriaParser.java
+++ src/main/java/com/asi/ext/api/product/transformers/ProductNumberCriteriaParser.java
@@ -6,21 +6,88 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-import com.asi.ext.api.radar.model.Product;
-import com.asi.ext.api.radar.model.ProductNumberConfigurations;
-import com.asi.ext.api.radar.model.ProductNumbers;
 import com.asi.ext.api.util.ApplicationConstants;
 import com.asi.ext.api.util.CommonUtilities;
+import com.asi.service.product.client.vo.ProductDetail;
+import com.asi.service.product.client.vo.ProductNumber;
+import com.asi.service.product.client.vo.ProductNumberConfiguration;
 
 public class ProductNumberCriteriaParser {
-    private final static Logger _LOGGER          = Logger.getLogger(ProductNumberCriteriaParser.class.getName());
+    private final static Logger LOGGER           = Logger.getLogger(ProductNumberCriteriaParser.class.getName());
     public ProductDataStore     productDataStore = new ProductDataStore();
 
+    public ProductDetail geProductNumbers(ProductDetail product,
+            List<com.asi.ext.api.service.model.ProductNumber> serviceProductNumbers, List<String> criteriaSetValueIds) {
+
+        // Product Numbers Starts Here
+        try {
+            // ProductNumberCriteriaParser productNumberCriteriaParser = new ProductNumberCriteriaParser();
+            List<ProductNumber> productNumberList = generateProductNumberCriterias(product, null, null);
+
+            if (null != product.getProductNumbers() && !product.getProductNumbers().isEmpty()) {
+                List<ProductNumber> productNumbers = getValidProductNumbers(product.getProductNumbers(), criteriaSetValueIds);
+                product.setProductNumbers(productNumbers);
+
+            } else {
+                product.setProductNumbers(new ArrayList<ProductNumber>());
+            }
+        } catch (Exception ex) {
+            product.setProductNumbers(new ArrayList<ProductNumber>());
+            LOGGER.error("Exception occured in Product Number Criteria processing : " + ex.getMessage());
+        }
+
+        return null;
+    }
+
+    /*
+     * 
+     * 
+     * // Product Numbers Starts Here
+     * try {
+     * ProductNumberCriteriaParser productNumberCriteriaParser = new ProductNumberCriteriaParser();
+     * if (isSimplifiedPG && existingProduct != null) {
+     * if (null != existingProduct.getProductNumbers() && !existingProduct.getProductNumbers().isEmpty()) {
+     * List<ProductNumbers> productNumbers = productNumberCriteriaParser.getValidProductNumbers(
+     * existingProduct.getProductNumbers(), criteriaSetValueIds, isSimplifiedPG);
+     * product.setProductNumbers(productNumbers);
+     * 
+     * } else {
+     * product.setProductNumbers(new ArrayList<ProductNumbers>());
+     * }
+     * } else if (!isSimplifiedPG && existingProduct != null) {
+     * product.setProductNumbers(productNumberCriteriaParser.generateProductNumberCriterias(product,
+     * existingProduct));
+     * if (null != existingProduct.getProductNumbers() && !existingProduct.getProductNumbers().isEmpty()) {
+     * List<ProductNumbers> productNumbers = productNumberCriteriaParser.getValidProductNumbers(
+     * existingProduct.getProductNumbers(), criteriaSetValueIds, isSimplifiedPG);
+     * product.setProductNumbers(productNumbers);
+     * 
+     * } else {
+     * product.setProductNumbers(new ArrayList<ProductNumbers>());
+     * }
+     * } else {
+     * product.setProductNumbers(productNumberCriteriaParser.generateProductNumberCriterias(product,
+     * existingProduct));
+     * if (existingProduct != null && existingProduct.getProductNumbers() != null
+     * && !existingProduct.getProductNumbers().isEmpty()) {
+     * List<ProductNumbers> productNumbers = productNumberCriteriaParser.getValidProductNumbers(
+     * existingProduct.getProductNumbers(), criteriaSetValueIds, isSimplifiedPG);
+     * product.setProductNumbers(productNumbers);
+     * 
+     * } else {
+     * product.setProductNumbers(new ArrayList<ProductNumbers>());
+     * }
+     * // product.setProductNumbers(new ArrayList<ProductNumbers>());
+     * }
+     * } catch (Exception ex) {
+     * product.setProductNumbers(new ArrayList<ProductNumbers>());
+     * LOGGER.error("Exception occured in Product Number Criteria processing : " + ex.getMessage());
+     * }
+     * 
+     * // Product Numbers Ends
+     */
     public static String getValidProductNumberriteriaCode(String[] productNumberCriterias) {
-        /*
-         * ArrayList<String> productNumberCriteria1List=new ArrayList();
-         * ArrayList<String> productNumberCriteria2List=new ArrayList();
-         */
+
         String temp[] = {};
         String productNumberCriteriaCode = "";
         for (String tempString : productNumberCriterias) {
@@ -37,12 +104,22 @@ public class ProductNumberCriteriaParser {
         }
         return null;
     }
+    
+    private void getProdNumMap(List<com.asi.ext.api.service.model.ProductNumber> pNumbers) {
+        List<String> pnoList = new ArrayList<String>();
+        int counter = 0;
+        for(com.asi.ext.api.service.model.ProductNumber serPno : pNumbers) {
+            
+        }
+    }
 
-    public ArrayList<ProductNumbers> generateProductNumberCriterias(Product crntProduct, Product existingProduct) {
-        ArrayList<ProductNumbers> finalProductNumbers = new ArrayList<>();
-        HashMap<String, ProductNumbers> crntProductNumberItems = null;
-        String[] criteriaPN1list = crntProduct.getProductNumbers().get(0).getProductNumberConfigurations().get(0)
-                .getCriteriaSetValueId().split(ApplicationConstants.PRICE_CRT_SPLITTER_BASECRITERIA);
+    @SuppressWarnings("unused")
+    public ArrayList<ProductNumber> generateProductNumberCriterias(ProductDetail crntProduct,
+            List<com.asi.ext.api.service.model.ProductNumber> serProductNumbers, ProductDetail existingProduct) {
+        ArrayList<ProductNumber> finalProductNumbers = new ArrayList<>();
+        HashMap<String, ProductNumber> crntProductNumberItems = null;
+        String[] criteriaPN1list = null;/*crntProduct.getProductNumbers().get(0).getProductNumberConfigurations().get(0)
+                .getCriteriaSetValueId().split(ApplicationConstants.PRICE_CRT_SPLITTER_BASECRITERIA)*/;
         String[] validCriteriaCodes = getValidCriteriaSets(criteriaPN1list);
         String firstCriteria = "";
         String secondCriteria = "";
@@ -68,8 +145,8 @@ public class ProductNumberCriteriaParser {
                 if (null != criteriaPN1 && criteriaPN1.length > 1
                         && !criteriaPN1[0].equalsIgnoreCase(ApplicationConstants.CONST_STRING_NULL_SMALL)
                         && !criteriaPN1[0].contains(",") && !criteriaPN1[1].contains(",")) {
-                    ProductNumbers crntProductNumber = null;
-                    List<ProductNumberConfigurations> productNumberConfigurationsAry = new ArrayList<>();
+                    ProductNumber crntProductNumber = null;
+                    List<ProductNumberConfiguration> productNumberConfigurationsAry = new ArrayList<>();
                     if (criteriaPN1[0].startsWith(firstCriteria)) {
                         criteriaSetId1 = getProductNumberIdByCriteria(criteriaPN1[0], crntProduct.getExternalProductId());
                     }
@@ -93,11 +170,11 @@ public class ProductNumberCriteriaParser {
                     if (null != crntProductNumber) {
                         finalProductNumbers.add(crntProductNumber);
                     } else {
-                        if (null != criteriaSetId1) {
-                            crntProductNumber = new ProductNumbers();
-                            ProductNumberConfigurations firstProductNumberConfigurations = new ProductNumberConfigurations();
-                            ProductNumberConfigurations secondProductNumberConfigurations = new ProductNumberConfigurations();
-                            crntProductNumber.setId("-" + (i + 1));
+                        if (null != criteriaSetId1) {/*
+                            crntProductNumber = new ProductNumber();
+                            ProductNumberConfiguration firstProductNumberConfigurations = new ProductNumberConfiguration();
+                            ProductNumberConfiguration secondProductNumberConfigurations = new ProductNumberConfiguration();
+                            //crntProductNumber.setId("-" + (i + 1));
                             crntProductNumber.setValue(productNumberValues[i]);
                             crntProductNumber.setProductId(crntProduct.getId());
                             firstProductNumberConfigurations.setCriteriaSetValueId(criteriaSetId1);
@@ -116,7 +193,7 @@ public class ProductNumberCriteriaParser {
                             }
                             crntProductNumber.setProductNumberConfigurations(productNumberConfigurationsAry);
                             finalProductNumbers.add(crntProductNumber);
-                        } else {
+                        */} else {
                             productDataStore.addErrorToBatchLogCollection(crntProduct.getExternalProductId().trim(),
                                     ApplicationConstants.CONST_BATCH_ERR_LOOKUP_VALUE_NOT_EXIST, "Product Number 1 Criteria "
                                             + criteriaPN1[0] + " is not valid");
@@ -129,12 +206,12 @@ public class ProductNumberCriteriaParser {
                         productDataStore.addErrorToBatchLogCollection(crntProduct.getExternalProductId().trim(),
                                 ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
                                 "Invalid Product Number Criteria 1 - Should not be Multiple Values" + criteriaPN1[0]);
-                        _LOGGER.info("Product Number Criteria 1 Should not be Multiple Values " + criteriaPN1[0]);
+                        LOGGER.info("Product Number Criteria 1 Should not be Multiple Values " + criteriaPN1[0]);
                     } else if (criteriaPN1[1].contains(",")) {
                         productDataStore.addErrorToBatchLogCollection(crntProduct.getExternalProductId().trim(),
                                 ApplicationConstants.CONST_BATCH_ERR_INVALID_VALUE,
                                 "Invalid Product Number Criteria 2 - Should not be Multiple Values" + criteriaPN1[1]);
-                        _LOGGER.info("Product Number Criteria 2 Should not be Multiple Values " + criteriaPN1[1]);
+                        LOGGER.info("Product Number Criteria 2 Should not be Multiple Values " + criteriaPN1[1]);
                     }
                 }
                 criteriaSetId1 = null;
@@ -144,12 +221,12 @@ public class ProductNumberCriteriaParser {
         return finalProductNumbers;
     }
 
-    private static HashMap<String, ProductNumbers> getExistingProductNumbersDetails(Product existingProduct) {
-        HashMap<String, ProductNumbers> existingProductNumbers = new HashMap<>();
-        ArrayList<ProductNumbers> existingProductNumbersList = (ArrayList<ProductNumbers>) existingProduct.getProductNumbers();
+    private static HashMap<String, ProductNumber> getExistingProductNumbersDetails(ProductDetail existingProduct) {
+        HashMap<String, ProductNumber> existingProductNumbers = new HashMap<>();
+        ArrayList<ProductNumber> existingProductNumbersList = (ArrayList<ProductNumber>) existingProduct.getProductNumbers();
         String productNumberKey1 = "";
         String productNumberKey2 = "";
-        for (ProductNumbers crntProductNumbers : existingProductNumbersList) {
+        for (ProductNumber crntProductNumbers : existingProductNumbersList) {
             productNumberKey1 = crntProductNumbers.getValue();
             productNumberKey2 = crntProductNumbers.getValue();
             if (crntProductNumbers.getProductNumberConfigurations().size() > 1) {
@@ -203,12 +280,12 @@ public class ProductNumberCriteriaParser {
         return criteriaSetValueId;
     }
 
-    protected List<ProductNumbers> getValidProductNumbers(List<ProductNumbers> existingProductNumbers,
-            List<String> criteriaSetValueIds, boolean isSimplifiedTemplate) {
+    protected List<ProductNumber> getValidProductNumbers(List<ProductNumber> existingProductNumbers,
+            List<String> criteriaSetValueIds) {
         // Check existing ProductNumber configuration exists or not, if not return the current copy
         if (existingProductNumbers != null && !existingProductNumbers.isEmpty()) {
 
-            if (isSimplifiedTemplate) { // if its a simplified template then validate and return the existing PNRConfiguration
+            if (true) { // if its a simplified template then validate and return the existing PNRConfiguration
                 existingProductNumbers = validateProductNumberConfiguration(existingProductNumbers, criteriaSetValueIds);
                 return existingProductNumbers;
             }
@@ -220,17 +297,17 @@ public class ProductNumberCriteriaParser {
         return null;
     }
 
-    private List<ProductNumbers> validateProductNumberConfiguration(List<ProductNumbers> productNumbers,
+    private List<ProductNumber> validateProductNumberConfiguration(List<ProductNumber> productNumbers,
             List<String> criteriaSetValueIds) {
         // If any of the participating criteria set value not existing in the current set,
         // then we must ignore that particular ProductNumberConfiguration.
-        List<ProductNumbers> processedProductNumbers = new ArrayList<ProductNumbers>();
+        List<ProductNumber> processedProductNumbers = new ArrayList<ProductNumber>();
 
-        for (ProductNumbers pNumber : productNumbers) {
+        for (ProductNumber pNumber : productNumbers) {
             if (pNumber != null && pNumber.getProductNumberConfigurations() != null
                     && !pNumber.getProductNumberConfigurations().isEmpty()) {
                 boolean isValid = true;
-                for (ProductNumberConfigurations pnrConfig : pNumber.getProductNumberConfigurations()) {
+                for (ProductNumberConfiguration pnrConfig : pNumber.getProductNumberConfigurations()) {
                     if (!criteriaSetValueIds.contains(pnrConfig.getCriteriaSetValueId())) {
                         isValid = false;
                         break;
@@ -246,20 +323,21 @@ public class ProductNumberCriteriaParser {
 
     }
 
-   /**
+    /**
      * Checks that the CriteriaSetValueId existing in the Criteria Collection.
      * 
      * @param configurations
      * @return true if its valid
      */
     /*
-    private boolean isValidPNRConfiguration(ProductNumberConfigurations configurations) {
-        if (configurations != null && !CommonUtilities.isValueNull(configurations.getCriteriaSetValueId())) {
-            // TODO : Check with DataStore
-        } else {
-            return false;
-        }
-
-        return false;
-    }*/
+     * private boolean isValidPNRConfiguration(ProductNumberConfigurations configurations) {
+     * if (configurations != null && !CommonUtilities.isValueNull(configurations.getCriteriaSetValueId())) {
+     * // TODO : Check with DataStore
+     * } else {
+     * return false;
+     * }
+     * 
+     * return false;
+     * }
+     */
 }
