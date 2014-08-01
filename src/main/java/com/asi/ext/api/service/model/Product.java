42cccad93f5209d64800edb77a5c11b06566d048
shravan <skumar@asicentral.com>
2014-08-01 13:45:39 +0530

    Product Service Model modifications

--- src/main/java/com/asi/ext/api/service/model/Product.java
+++ src/main/java/com/asi/ext/api/service/model/Product.java
@@ -11,9 +11,12 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
+@JsonInclude(Include.NON_NULL)
 public class Product {
 
     @JsonProperty("ExternalProductId")
